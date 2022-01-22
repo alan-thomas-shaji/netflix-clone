c0b65e2ccbb3a13c28486fb0719cc160ddba08e7
Alan Thomas Shaji <50659499+Captain-shaji-stark@users.noreply.github.com>
2022-01-23 00:43:13 +0530

    Started arranging the rows, display flex made my day ;)

--- src/Row.js
+++ src/Row.js
@@ -1,19 +1,37 @@
 import React, { useEffect, useState } from 'react'
 import axios from './axios';
+import './App.css';
 
-function Row({ title }) {
+const imageBase = `https://image.tmdb.org/t/p/original/`;
+
+function Row({ title, fetchURL }) {
 
     const [movies, setMovies] = useState([]);
 
     useEffect(()=>{
+      async function fetddata(){
+        const request = await axios.get(fetchURL);
+        setMovies(request.data.results);
+        return request;
+      }
+      fetddata();
+    },[fetchURL]);
 
-    },[]);
+    console.log(movies);
 
   return (
-  <div>
-      {/* TITLE */}
+  <div className='row'>
+                                                    {/* TITLE */}
       <h2>{title}</h2> 
-      {/* CONTAINER FOR TILES */}
+                                                    {/* CONTAINER FOR TILES */}
+      <div className="movies-container">
+
+        {
+        movies.map(movie =>(
+          <img className='poster'
+           src={`${imageBase}${movie.poster_path}`} alt={movie.name} />
+        ))}
+      </div>
 
   </div>
   )
